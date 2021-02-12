c48ee6993ebb0f2c685be2ac2f9e12777918144f
Januario86 <januario86fp@gmail.com>
2021-02-12 09:41:16 -0300

    Commit inicial htmlTopdf download

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import * as jsPDF from 'jspdf';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'html-to-pdf';
+
+  @ViewChild('content') content: ElementRef;
+
+  public downloadPDF() {
+    console.log('entrei download');
+    let doc = new jsPDF();
+
+    let specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+
+    let content = this.content.nativeElement;
+
+    doc.fromHTML(content.innerHTML,15,15,{
+      'width': 190,
+      'elementHandlers': specialElementHandlers
+    });
+
+    doc.save('test.pdf');
+  }
+
+ 
 }
